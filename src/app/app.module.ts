03e90d2d71673150e38346049344a930e1885854
bhavesh <bhaveshgupta41@gmail.com>
2023-05-03 15:02:31 +0530

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScreenOneComponent } from './screen-one/screen-one.component';
+import { ScreenTwoComponent } from './screen-two/screen-two.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenOneComponent,
+    ScreenTwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
